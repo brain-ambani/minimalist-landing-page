6915c86ec69c1083f8868ee2ebf7e571a16e9db0
brian-ambani <brainambani1@gmail.com>
2024-06-13 12:13:09 +0300

    updated the folder structure using route group approach

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,9 +1,9 @@
 import { ModeToggle } from "@/components/themeToggle";
 
-export default function Home() {
+export default function DashboardPage() {
   return (
     <main className="flex justify-between">
-      <h1>Hello, world!</h1>
+      <h1 className="text-green-500">Dashboard(Protected)</h1>
       <ModeToggle />
     </main>
   );
