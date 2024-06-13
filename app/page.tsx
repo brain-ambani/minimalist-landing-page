cd9a27ebf96dba1bfa3a9d11b23dda48adb6c0d1
brian-ambani <brainambani1@gmail.com>
2024-06-13 12:00:55 +0300

    updated the new project

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { ModeToggle } from "@/components/themeToggle";
+
+export default function Home() {
+  return (
+    <main className="flex justify-between">
+      <h1>Hello, world!</h1>
+      <ModeToggle />
+    </main>
+  );
+}
