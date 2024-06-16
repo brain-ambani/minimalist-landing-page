352a5d141d781951f75b2cedfa6900b45e1cf43f
brain-ambani <brainambani1@gmail.com>
2024-06-16 22:30:42 +0300

    deleted evrything

--- app/(home)/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main className="flex justify-between">
-      <h1 className="text-green-500">This is the home page(Unprotected)</h1>
-    </main>
-  );
-}
